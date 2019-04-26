797de554fb5f164ec0ca2b3fb59771aac9c5a2cd
tigerfarm <tigerfarm@gmail.com>
2019-04-26 15:36:06 -0700

    updates

--- twiml/say.java
+++ twiml/say.java
@@ -1,13 +1,18 @@
+package twiml;
 // Documentation: https://www.twilio.com/docs/voice/twiml/say
+// https://docs.aws.amazon.com/polly/latest/dg/voicelist.html
 import com.twilio.twiml.VoiceResponse;
 import com.twilio.twiml.voice.Say;
 import com.twilio.twiml.TwiMLException;
 public class say {
     public static void main(String[] args) {
-        String toSay = "Bonjour";
+        // String toSay = "Bonjour";
+        String toSay = "Ciao, come stai?";
         Say say = new Say.Builder(toSay)
-                .voice(Say.Voice.WOMAN)
-                .language(Say.Language.FR_FR).build();
+                .voice(Say.Voice.POLLY_CARLA)
+                // .voice(Say.Voice.WOMAN)
+                // .language(Say.Language.FR_FR)
+                .build();
         VoiceResponse response = new VoiceResponse.Builder().say(say).build();
         try {
             System.out.println(response.toXml());

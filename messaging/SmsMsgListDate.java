cb5568c61746e3b6f58e7b8abf7c0f7b61953e64
tigerfarm <tigerfarm@gmail.com>
2020-07-22 17:42:44 -0700

    updates

--- messaging/SmsMsgListDate.java
+++ messaging/SmsMsgListDate.java
@@ -9,12 +9,11 @@ import com.twilio.base.ResourceSet;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import org.joda.time.DateTime;
-import com.google.common.collect.Range;
 
 public class SmsMsgListDate {
 
-    private static final String ACCOUNT_SID = System.getenv("ACCOUNT_SID");
-    private static final String AUTH_TOKEN = System.getenv("AUTH_TOKEN");
+    private static final String ACCOUNT_SID = System.getenv("MASTER_ACCOUNT_SID");
+    private static final String AUTH_TOKEN = System.getenv("MASTER_AUTH_TOKEN");
 
     public static void main(String[] args) {
         String theClassName = "GetMsgListSms";
@@ -23,15 +22,11 @@ public class SmsMsgListDate {
         ResourceSet<Message> messages;
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        String thisPhoneNumber = System.getenv("PHONE_NUMBER2");
-        Range<DateTime> rangeDateSent;
-        rangeDateSent = null;
-        rangeDateSent.lowerEndpoint();
-
+        String thisPhoneNumber = System.getenv("MASTER_PHONE_NUMBER_2");
         System.out.println("++ List inbound messages: " + thisPhoneNumber);
         messages = Message.reader()
                 .setTo(new PhoneNumber(thisPhoneNumber))
-                .setDateSent(DateTime.parse("2019-02-06T00:00:00.000Z"))
+                .setDateSent(DateTime.parse("2020-07-17T00:00:00.000Z"))
                 .read();
         for (Message message : messages) {
             System.out.print("+ from:" + message.getFrom());
@@ -48,11 +43,12 @@ public class SmsMsgListDate {
         System.out.println("++ List outbound messages: " + thisPhoneNumber);
         messages = Message.reader()
                 .setFrom(new PhoneNumber(thisPhoneNumber))
-                .setDateSent(DateTime.parse("2019-02-06"))
+                .setDateSent(DateTime.parse("2020-07-17"))
                 .read();
         for (Message message : messages) {
             System.out.print("+ from:" + message.getFrom());
             System.out.print(", to:" + message.getTo());
+            System.out.print(", " + message.getDateSent());
             System.out.print(", " + message.getDateSent());
             System.out.print(", " + message.getStatus());
             System.out.print(", " + message.getPrice());

5e51ccec652a7eade6eb06fef14ec5c7e1e8442d
tigerfarm <tigerfarm@gmail.com>
2021-07-02 12:26:31 -0700

    updates

--- javatwsamples/base64sample.java
+++ javatwsamples/base64sample.java
@@ -1,12 +1,13 @@
 package javatwsamples;
 
+import java.util.Arrays;
 import java.util.Base64;
 
 public class base64sample {
 
     public static void main(String[] args) {
         System.out.println("+++ Start: Base64 encoding.");
-        String aString = "test input";
+        String aString = "abc";
         System.out.println("+ String: "
                 + "+ Encoded: "
                 + Base64.getEncoder().encodeToString(aString.getBytes())
@@ -16,6 +17,21 @@ public class base64sample {
                 + Base64.getEncoder().encodeToString(
                         String.format("%s:%s", System.getenv("MASTER_ACCOUNT_SID"), System.getenv("MASTER_AUTH_TOKEN")).getBytes()
                 )
+        );
+        
+        System.out.println("+ How to decode a string.");
+        String bString = "abcdef";
+        String bStringEcoded = Base64.getEncoder().encodeToString(bString.getBytes());
+        System.out.println(
+                "++ Original string: " 
+                + bString
+                + ", Encoded: " 
+                + bStringEcoded
+                + ", Decoded: "
+                + new String(Base64.getDecoder().decode(
+                        bStringEcoded
+                ))
+                
         );
         System.out.println("+++ Exit.");
     }

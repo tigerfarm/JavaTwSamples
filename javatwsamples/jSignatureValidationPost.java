98324b4d26ee4ec5b29e87a868a8f9de6f43779d
Stacy David Thurston <tigerfarm@gmail.com>
2022-03-25 11:04:22 -0700

    update

--- javatwsamples/jSignatureValidationPost.java
+++ javatwsamples/jSignatureValidationPost.java
@@ -14,12 +14,13 @@ public class jSignatureValidationPost {
         // Initialize the validator
         String AUTH_TOKEN = System.getenv("MASTER_AUTH_TOKEN");
         RequestValidator validator = new RequestValidator(AUTH_TOKEN);
-        // The Twilio request URL
+        // The actual Twilio request URL
         String url = "https://example.com/myapp";
-        // The post variables in Twilio's request
+        // The post variables in the Twilio request
         Map<String, String> params = new HashMap<>();
         params.put("CallSid", "CA1234567890ABCDE");
-        params.put("Caller", "+12349013030");
+        params.put("Caller", "+16505551111");
+        // ... all the parameters ...
         System.out.println("+ params: " + params);
         String twilioSignature = "0/KCTR6DLpKmkAf8muzZqo1nDgQ=";
         System.out.println("+ Signature validation is: " + validator.validate(url, params, twilioSignature));

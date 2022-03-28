51d00af9f699af477d5bca25c84c052e44898711
Stacy David Thurston <tigerfarm@gmail.com>
2022-03-28 14:40:58 -0700

    update

--- javatwsamples/jSignatureValidationPost.java
+++ javatwsamples/jSignatureValidationPost.java
@@ -5,6 +5,7 @@ package javatwsamples;
     java -cp .:twilio-8.18.0-jar-with-dependencies.jar jSignatureValidationPost
  */
 import com.twilio.security.RequestValidator;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,13 +17,36 @@ public class jSignatureValidationPost {
         RequestValidator validator = new RequestValidator(AUTH_TOKEN);
         // The actual Twilio request URL
         String url = "https://example.com/myapp";
-        // The post variables in the Twilio request
-        Map<String, String> params = new HashMap<>();
-        params.put("CallSid", "CA1234567890ABCDE");
-        params.put("Caller", "+16505551111");
-        // ... all the parameters ...
+        String twilioSignature = "0/KCTR6DLpKmkAf8muaAqo1nDgQ=";
+        System.out.println("+ url :       " + url);
+        System.out.println("+ signature : " + twilioSignature);
+        //
+        // The POST body in the Twilio request.
+        // The requestString is the Twilio POST body RAW data string.
+        String requestString = "ToCountry=US&ToState=CA&SmsMessageSid=SM60566ee207e123ee2765abcc55c9b9c9&NumMedia=0&ToCity=SAN+BRUNO&FromZip=94030&SmsSid=SM60566ee207e123ee2765abcc55c9b9c9&FromState=CA&SmsStatus=received&FromCity=SAN+BRUNO&Body=hi+there%2C+2%2B3%3D4&FromCountry=US&To=%2B16505551366&ToZip=94030&NumSegments=1&ReferralNumMedia=0&MessageSid=SM60566ee207e123ee2765abcc55c9b9c9&AccountSid=ACae0e356ccba96d16d8d4f6f9518684a3&From=%2B16505558893&ApiVersion=2010-04-01";
+        // Haved the parameters loaded into Map and URL decoded.
+        Map<String, String> params = new HashMap<String, String>();
+        String[] parts = requestString.split("&");
+        for (String part : parts) {
+            String[] keyVal = part.split("="); // The equal separates attribute names and values.
+            if (keyVal.length > 1) {
+                params.put(keyVal[0], URLDecoder.decode(keyVal[1]));
+            }
+        }
         System.out.println("+ params: " + params);
-        String twilioSignature = "0/KCTR6DLpKmkAf8muzZqo1nDgQ=";
+        //
         System.out.println("+ Signature validation is: " + validator.validate(url, params, twilioSignature));
+    }
+
+    public static Map<String, String> urlDecodeIntoMap(String requestString) {
+        Map<String, String> params = new HashMap<String, String>();
+        String[] parts = requestString.split("&");
+        for (String part : parts) {
+            String[] keyVal = part.split("="); // The equal separates key and values
+            if (keyVal.length > 1) {
+                params.put(keyVal[0], URLDecoder.decode(keyVal[1]));
+            }
+        }
+        return params;
     }
 }
---
+++ javatwsamples/jSignatureValidationPost1.java
@@ -0,0 +1,31 @@
+package javatwsamples;
+/*
+    https://www.twilio.com/docs/usage/security
+    javac -cp twilio-8.18.0-jar-with-dependencies.jar jSignatureValidationPost.java
+    java -cp .:twilio-8.18.0-jar-with-dependencies.jar jSignatureValidationPost
+ */
+import com.twilio.security.RequestValidator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class jSignatureValidationPost1 {
+
+    public static void main(String[] args) {
+        // Initialize the validator
+        String AUTH_TOKEN = System.getenv("MASTER_AUTH_TOKEN");
+        RequestValidator validator = new RequestValidator(AUTH_TOKEN);
+        // The actual Twilio request URL
+        String url = "https://example.com/myapp";
+        //
+        // The POST body in the Twilio request.
+        // Note, the parameters need to URL decoded.
+        Map<String, String> params = new HashMap<>();
+        params.put("CallSid", "CA1234567890ABCDE");
+        params.put("Caller", "+16505551111");
+        // ... all the parameters ...
+        System.out.println("+ params: " + params);
+        //
+        String twilioSignature = "0/KCTR6DLpKmkAf8muzZqo1nDgQ=";
+        System.out.println("+ Signature validation is: " + validator.validate(url, params, twilioSignature));
+    }
+}
